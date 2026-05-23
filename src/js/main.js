cb380d0c988f5994d3796ca3ede0880ca43800fb
Emmaonukwue <onukwueemma@gmail.com>
2026-05-23 16:44:17 +0100

    ran format and lint in week 2 individual activity

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
-import ProductData from './ProductData.mjs';
-import ProductList from './ProductList.mjs';
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
 
 const productData = new ProductData("tents");
-const listElement = document.querySelector('.product-list');
+const listElement = document.querySelector(".product-list");
 
 async function initProductList() {
   await productData.getData(); // Ensure data is loaded
-  const productList = new ProductList('Tents', productData, listElement);
+  const productList = new ProductList("Tents", productData, listElement);
   productList.init();
 }
 
