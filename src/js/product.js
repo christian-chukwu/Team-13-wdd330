0c5b0f88928518b55b5037d3445c87add0c0952e
Emmaonukwue <onukwueemma@gmail.com>
2026-05-23 12:36:14 +0100

    completed week 2 Team Activity

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,24 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
+const productId = getParam('product');
 const dataSource = new ProductData("tents");
 
+
+console.log(productId);
+console.log(dataSource.findProductById(productId));
+
+const product = new ProductDetails(productID, dataSource);
+product.init();
+
+/*
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
 }
+  */
+
+/*
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
@@ -16,3 +29,5 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+
+  */
