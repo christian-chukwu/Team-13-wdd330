9db27e7e057d07cc215c9bc55438515913c850be
Danson Nganga <Dansonnganga019@gmail.com>
2026-05-27 18:12:24 +0300

    discount indicator

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,12 +1,42 @@
-import { renderListWithTemplate } from './utils.mjs';
+import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
+  const isDiscounted = product.FinalPrice < product.SuggestedRetailPrice;
+
+  const discountPercent = Math.round(
+    ((product.SuggestedRetailPrice - product.FinalPrice) /
+      product.SuggestedRetailPrice) *
+      100,
+  );
+
   return `<li class="product-card">
-    <a href="product_pages/?product=${product.Id}">
-      <img src="${product.Image}" alt="Image of ${product.Name} ">
+    <a href="/product_pages/index.html?product=${product.Id}">
+      
+      ${
+        isDiscounted
+          ? `<p class="discount-badge">${discountPercent}% OFF</p>`
+          : ""
+      }
+
+      <img 
+        src="${product.Images.PrimaryMedium}" 
+        alt="Image of ${product.Name}"
+      >
+
       <h2 class="card__brand">${product.Brand.Name}</h2>
+
       <h3 class="card__name">${product.NameWithoutBrand}</h3>
-      <p class="product-card__price">$${product.ListPrice}</p>
+
+      <p class="product-card__price">
+        $${product.FinalPrice}
+      </p>
+
+      ${
+        isDiscounted
+          ? `<p class="original-price">$${product.SuggestedRetailPrice}</p>`
+          : ""
+      }
+
     </a>
   </li>`;
 }
@@ -19,15 +49,11 @@ export default class ProductList {
   }
 
   async init() {
-    const list = await this.dataSource.getData();
+    const list = await this.dataSource.getData(this.category);
     this.renderList(list);
   }
 
   renderList(list) {
-    return renderListWithTemplate(
-      productCardTemplate,
-      this.listElement,
-      list,
-    );
+    return renderListWithTemplate(productCardTemplate, this.listElement, list);
   }
 }
